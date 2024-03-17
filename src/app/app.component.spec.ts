9bd520d74758dc4720b5d258d30c1ab0a2bf007d
Darian Camacho <delgadodarian2003@gmail.com>
2024-03-17 16:36:31 -0600

    Login and Register

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'empathylink-app' title`, () => {
+  it(`should have the 'pokedex-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('empathylink-app');
+    expect(app.title).toEqual('pokedex-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, empathylink-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokedex-app');
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp} from '@angular/fire/app';
+import { getAuth, provideAuth  } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC07-l5bGPNSKTxiPfn-JeBR_-a5uBQLlE",
+  authDomain: "empathylinkdb1-0.firebaseapp.com",
+  projectId: "empathylinkdb1-0",
+  storageBucket: "empathylinkdb1-0.appspot.com",
+  messagingSenderId: "98425988466",
+  appId: "1:98425988466:web:1c3084bf39bf4f19ba69eb",
+  measurementId: "G-P25KXH83S9"
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideAuth(() => getAuth()),
+    ]),
+  ],
 };
